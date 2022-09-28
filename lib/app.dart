e2e55ce8c437594cdfa19b4e41e558754c9d868e
Chris Au <chrisau.au@edg.com.au>
2022-09-28 17:39:19 +1000

    adding http

--- lib/app.dart
+++ lib/app.dart
@@ -2,9 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import './home.dart';
+import 'models/pixa_results.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class PixaSearchApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _PixaSearchAppState();
@@ -12,18 +15,56 @@ class PixaSearchApp extends StatefulWidget {
 }
 
 class _PixaSearchAppState extends State<PixaSearchApp> {
+  final myController = TextEditingController();
+  late Future<PixaResults> futurePixaResults;
 
-  String _str = '';
+  Future<PixaResults> fetchPixaResults() async {
+    print('calling method');
 
-  void _changeValue(String value){
-    print('this is changed $value');
-    setState((){
-      // _str = value;
+    final response = await http.get(Uri.parse(
+        'https://pixabay.com/api/?key=30140123-68f55f4801af03afee214a954&q=${myController.text}&image_type=photo&pretty=true'));
+
+    print('calling method ${response.statusCode}');
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      var x = PixaResults.fromJson(jsonDecode(response.body));
+      print('${x}');
+      return x;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: implement initState
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    // This also removes the _printLatestValue listener.
+    myController.dispose();
+    super.dispose();
+  }
+
+  void _printLatestValue() {
+    print('Second text field ${myController.text}');
+    setState(() {
+      if (myController.text.length > 3) {
+        futurePixaResults = fetchPixaResults();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print('redraw');
     return CupertinoApp(
       localizationsDelegates: [
         DefaultMaterialLocalizations.delegate,
@@ -31,7 +72,7 @@ class _PixaSearchAppState extends State<PixaSearchApp> {
         DefaultWidgetsLocalizations.delegate,
       ],
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: PixabayHomePage(_str, _changeValue),
+      home: PixabayHomePage(myController, this.futurePixaResults),
     );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'models/pixa_results.dart';
 
 class PixabayHomePage extends StatelessWidget {
+  Future<PixaResults> futurePixaResults;
+  var myController;
 
-  String str = '';
-  Function changeValue;
+  PixabayHomePage(this.myController, this.futurePixaResults);
 
-  PixabayHomePage(this.str, this.changeValue);
-
-  final TextEditingController _textController = TextEditingController(text: '');
+  // final TextEditingController _textController = TextEditingController(text: '');
   List items = List.generate(50, (index) => "This is item $index");
 
   @override
@@ -19,49 +19,48 @@ class PixabayHomePage extends StatelessWidget {
           child: Container(
             height: 150,
             child: CupertinoNavigationBar(
-              // backgroundColor: Colors.white,
+                // backgroundColor: Colors.white,
                 middle: Column(
-                  children: [
-                    Container(
-                      child:Text('Pixabay'),
-                      margin: EdgeInsets.only(bottom: 20),
-                    ),
-                    Container(
-                        margin: EdgeInsets.only(),
-                        child: CupertinoSearchTextField(
-                          controller: _textController,
-                          placeholder: 'Search',
-                          onChanged: (String value) {
-                            print('this is value $value');
-                            // if(value.length >= 4) {
-                             changeValue(value);
-                            // }
-                          },
-                        )
-                    ),
-                  ],)
-            ),
-          )
-      ),
+              children: [
+                Container(
+                  child: Text('Pixabay'),
+                  margin: EdgeInsets.only(bottom: 20),
+                ),
+                Container(
+                    margin: EdgeInsets.only(),
+                    child: CupertinoSearchTextField(
+                      controller: myController,
+                      placeholder: 'Search',
+                      // onChanged: (String value) {
+                      //   str = value;
+                      //   print('this is value $str');
+                      //   // if(value.length >= 4) {
+                      //   //  changeValue(value);
+                      //   // }
+                      // },
+                    )),
+              ],
+            )),
+          )),
       body: CupertinoScrollbar(
-        child: ListView.builder(
-          itemBuilder:(context, index) {
-            return Material(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ListTile(
-                      title: Text(str),
-                    ),
-                    Divider(
-                      height: 100,
-                    )
-                  ],
-                )
-            );
-          },
-          itemCount: items.length,
-        ),
+        child: FutureBuilder<Album>(
+          future: futureAlbum,
+        // ListView.builder(
+        //   itemBuilder: (context, index) {
+        //     final Hits hits = this.futurePixaResults.hits![index];
+        //     return Material(
+        //         child: Column(
+        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //       children: [
+        //         Image.network(''),
+        //         Divider(
+        //           height: 100,
+        //         )
+        //       ],
+        //     ));
+        //   },
+        //   itemCount: items.length,
+        // ),
       ),
     );
   }
